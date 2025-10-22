16fb1bc32b2e6e95c8c833eb34314ab967dddde8
Jay <17236763+jaspreetbhamra@users.noreply.github.com>
2025-10-22 16:16:46 -0700

    bug fix

--- scripts/training/finetune_base.py
+++ scripts/training/finetune_base.py
@@ -271,7 +271,7 @@ class StoryForgeTrainer:
             logging_steps=self.config.logging_steps,
             save_steps=self.config.save_steps,
             eval_steps=self.config.eval_steps,
-            evaluation_strategy="steps",
+            eval_strategy="steps",
             save_strategy="steps",
             load_best_model_at_end=True,
             metric_for_best_model="eval_loss",
